423be5a4b12484c9e2fe8fc285642fd66800108b
samer abdulla <smsssamer@live.com>
2015-12-21 22:02:36 +0400

    class

---
+++ xpremaAccountingV/Xprema.ConnectorLayer/CommandItemCategory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xprema.DataLayer;
+
+namespace Xprema.ConnectorLayer
+{
+    public class CommandItemCategory
+    {
+        public bool NewItemCategory(db.ItemCategoryRow row)
+        {
+            var r = XCommander._db.ItemCategory.NewItemCategoryRow();
+            row.CategoryName = row.CategoryName;
+            row.Description = row.Description;
+            XCommander._db.ItemCategory.AddItemCategoryRow(r);
+            XCommander.Commit();
+            return true;
+            
+        }
+
+        public bool EditItemCategory(db.ItemCategoryRow row)
+        {
+            var r = XCommander._db.ItemCategory.FindByID(row.ID);
+            r.CategoryName = row.CategoryName;
+            r.Description = row.Description;
+            XCommander.Commit();
+            return true;
+        }
+
+        public bool DeleteItemCategory(db.ItemCategoryRow row)
+        {
+            var r = XCommander._db.ItemCategory.FindByID(row.ID);
+            r.Delete();
+            XCommander.Commit();
+            return true;
+        }
+
+        public List<db.ItemCategoryRow> ListCategoryRows()
+        {
+            return XCommander._db.ItemCategory.ToList();
+        }
+    }
+}
