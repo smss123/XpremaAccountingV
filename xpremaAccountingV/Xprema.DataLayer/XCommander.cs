9a40602b49882cd5c402aba00688e6f9e367b3f0
samer abdulla <smsssamer@live.com>
2015-12-21 21:03:45 +0400

    init lacss

---
+++ xpremaAccountingV/Xprema.DataLayer/XCommander.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Xprema.DataLayer
+{
+    public class XCommander
+    {
+        public static db _db = new db();
+        public static string Path = Application.StartupPath + @"\Data.xprema";
+
+        public static bool CreateFile()
+        {
+            if (!File.Exists(Path))
+            {
+                _db.WriteXmlSchema(Path);
+                return true;
+            }
+            ReadFile();
+            return true;
+        }
+
+        public static bool ReadFile()
+        {
+            try
+            {
+                _db = new db();
+
+                _db.ReadXml(Path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                CreateFile();
+                ReadFile();
+                return true;
+            }
+        }
+
+        public static bool Commit()
+        {
+            _db.WriteXml(Path);
+
+            return true;
+        }
+    }
+}
